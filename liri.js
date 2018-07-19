b2a5e92dc74391ddc7190e2cd9fd62815de1fcf8
Daemone <kylebehm@gmail.com>
2018-07-19 16:58:21 -0400

    skeleton

--- liri.js
+++ liri.js
@@ -1,22 +1,70 @@
 // requirements and authentication
 require("dotenv").config();
+var keys = require("./keys")
+
 var fs = require("fs")
 var request = require("request")
 
 // ---Spotify--------
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-  });
+var spotify = new Spotify(keys.spotify);
 
 // ---Twitter--------
 var Twitter = require('twitter');
 
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
+var client = new Twitter(keys.twitter);
+
+// get arguments
+// function input
+var liriFunction = process.argv[2];
+// string input
+var liriString = process.argv[3];
+
+// create a function
+var chooseFunction = function () {
+    // create a switch
+    switch (liriFunction) {
+
+        case "my-tweets":
+          return twitterFunction();
+      
+        case "spotify-this-song":
+          return spotifyFunction();
+      
+        case "movie-this":
+          return movieFunction();
+      
+        case "do-what-it-says":
+          return randomFunction();
+      
+        default:
+          return console.log("Invalid input. Try again.") ;
+        }
+}
+
+// individual functions
+
+// "my-tweets"
+var twitterFunction = function () {
+    console.log('test +++')
+}
+
+// "spotify-this"
+var spotifyFunction = function () {
+
+}
+
+// "movie-this"
+var movieFunction = function () {
+
+}
+
+// "do-what-it-says"
+var randomFunction = function () {
+
+}
+
+
+// call input function
+chooseFunction();
